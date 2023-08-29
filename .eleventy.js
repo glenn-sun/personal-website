110a97491a71d4578d013cd5e9a568c3413c7ce2
Glenn Sun <glennsun@ucla.edu>
2023-08-28 21:53:02 -0700

    2023-08-28 update + remove eleventy dependency

--- .eleventy.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('css')
-  eleventyConfig.addPassthroughCopy('docs')
-  eleventyConfig.addPassthroughCopy('img')
-
-  return {
-    passthroughFileCopy: true
-  }
-}
